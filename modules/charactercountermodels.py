95dc00cd89b95b9eefa0ae19cad28bfb48e886fa
Joakim Jensen <joakimje@srl-login1.cm.cluster>
2022-11-22 14:56:32 +0100

    padding setting to character counter

--- modules/charactercountermodels.py
+++ modules/charactercountermodels.py
@@ -2,7 +2,10 @@ import torch
 
 import torch.nn as nn
 
+from torchsummary import summary
+
 from modules.pyramidpooling import TemporalPyramidPooling
+# from pyramidpooling import TemporalPyramidPooling
 
 from timm.models.registry import register_model
 
@@ -12,50 +15,46 @@ __all__ = [
 
 
 class CharacterCounterNet(nn.Module):
-    def __init__(self, outputs:int=17):
+    def __init__(self, padding: str, pyramid_pooling=False, outputs:int=17):
         super().__init__()
 
+        if not (padding == 'same' or padding == 'valid'):
+            raise Exception('Padding has to be same or valid')
+
         self.conv = nn.Sequential(
-            nn.Conv2d(3, 64, (3, 3), padding='same'),
+            nn.Conv2d(3, 64, (3, 3), padding=padding),
             nn.ReLU(),
-            nn.Conv2d(64, 64, (3, 3), padding='same'),
-            nn.ReLU(),
-            nn.MaxPool2d((2, 2), stride=2),
-            nn.Conv2d(64, 128, (3, 3), padding='same'),
-            nn.ReLU(),
-            nn.Conv2d(128, 128, (3, 3), padding='same'),
+            nn.Conv2d(64, 64, (3, 3), padding=padding),
             nn.ReLU(),
             nn.MaxPool2d((2, 2), stride=2),
-            nn.Conv2d(128, 256, (3, 3), padding='same'),
+            nn.Conv2d(64, 128, (3, 3), padding=padding),
             nn.ReLU(),
-            nn.Conv2d(256, 256, (3, 3), padding='same'),
+            nn.Conv2d(128, 128, (3, 3), padding=padding),
             nn.ReLU(),
-            nn.Conv2d(256, 256, (3, 3), padding='same'),
+            nn.MaxPool2d((2, 2), stride=2),
+            nn.Conv2d(128, 256, (3, 3), padding=padding),
             nn.ReLU(),
-            nn.Conv2d(256, 256, (3, 3), padding='same'),
+            nn.Conv2d(256, 256, (3, 3), padding=padding),
             nn.ReLU(),
-            nn.Conv2d(256, 256, (3, 3), padding='same'),
+            nn.Conv2d(256, 256, (3, 3), padding=padding),
             nn.ReLU(),
-            nn.Conv2d(256, 256, (3, 3), padding='same'),
+            nn.Conv2d(256, 256, (3, 3), padding=padding),
             nn.ReLU(),
-            nn.Conv2d(256, 512, (3, 3), padding='same'),
+            nn.Conv2d(256, 256, (3, 3), padding=padding),
             nn.ReLU(),
-            nn.Conv2d(512, 512, (3, 3), padding='same'),
+            nn.Conv2d(256, 256, (3, 3), padding=padding),
             nn.ReLU(),
-            nn.Conv2d(512, 512, (3, 3), padding='same'),
+            nn.Conv2d(256, 512, (3, 3), padding=padding),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, (3, 3), padding=padding),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, (3, 3), padding=padding),
             nn.ReLU(),
         )
-
+        
         self.temporal_pool = TemporalPyramidPooling([1, 2, 5])
 
         self.head = nn.Sequential(
-            nn.Linear(4096, 4096),
-            nn.ReLU(),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(),
-            nn.Dropout(),
-
             nn.Linear(4096, outputs),
             nn.Softmax(1)
         )
@@ -69,14 +68,16 @@ class CharacterCounterNet(nn.Module):
         return self.head(x)
 
 
-@register_model()
+@register_model
 def PHOSCnet_character_counter(outputs=17, **kwargs):
-    return CharacterCounterNet(outputs)
+    return CharacterCounterNet('same', outputs)
 
 if __name__ == '__main__':
-    model = CharacterCounterNet(17)
+    model = CharacterCounterNet(padding='same', outputs=17).to('cuda')
 
-    x = torch.randn(5, 50, 250, 3).view(-1, 3, 50, 250)
+    summary(model, (3, 50, 250))
+
+    x = torch.randn((5, 3, 50, 250)).to('cuda')
 
     y = model(x)
 
